535626c5dffc724ac77386cdad7724bfb30b2b32
Maxime Lemaitre <maxime.lemaitre@outlook.com>
2016-01-25 15:12:18 +0100

    CleanUp TypeFactory

--- src/Nullify/TypeFactory.cs
+++ src/Nullify/TypeFactory.cs
@@ -27,16 +27,16 @@ namespace Nullify
 
             foreach (var type in types)
             {
-                BuildMethods(typeBuilder, type);
-                BuildEvents(typeBuilder, type);
-                BuildProperties(typeBuilder, type);
+                AddMethods(typeBuilder, type);
+                AddEvents(typeBuilder, type);
+                AddProperties(typeBuilder, type);
             }
 
             var newtype = typeBuilder.CreateType();
             return newtype;
         }
 
-        private void BuildProperties(TypeBuilder typeBuilder, Type nullifiedType)
+        private void AddProperties(TypeBuilder typeBuilder, Type nullifiedType)
         {
             var properties = nullifiedType.GetProperties();
 
@@ -62,7 +62,6 @@ namespace Nullify
                     il.Emit(OpCodes.Ldloc_0);
                     il.Emit(OpCodes.Ret);
                     propertyBuilder.SetGetMethod(getter);
-                    //typeBuilder.DefineMethodOverride(getter, property.GetGetMethod());
                 }
 
                 if (property.CanWrite)
@@ -78,75 +77,45 @@ namespace Nullify
                     il.Emit(OpCodes.Ret);        // Push "value" on the stack
 
                     propertyBuilder.SetSetMethod(setter);
-                    //typeBuilder.DefineMethodOverride(setter, property.GetSetMethod());
                 }
-
-
-                //generate setter method
-
-
-
-
             }
         }
 
-        private void BuildEvents(TypeBuilder typeBuilder, Type nullifiedType)
+        private void AddEvents(TypeBuilder typeBuilder, Type nullifiedType)
         {
             var events = nullifiedType.GetEvents();
 
             foreach (var evt in events)
             {
-                //var eventBuilder = typeBuilder.DefineEvent(evt.Name,
-                //    evt.Attributes, evt.EventHandlerType);
-
                 // Event methods attributes
-                MethodAttributes eventMethodAttr = MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.Virtual | MethodAttributes.NewSlot | MethodAttributes.Final | MethodAttributes.SpecialName;
-                MethodImplAttributes eventMethodImpAtr = MethodImplAttributes.Managed;
+                var eventMethodAttr = MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.Virtual | MethodAttributes.NewSlot | MethodAttributes.Final | MethodAttributes.SpecialName;
+                var eventMethodImpAtr = MethodImplAttributes.Managed;
 
-                string qualifiedEventName = evt.Name;
-                string addMethodName = string.Format("add_{0}", evt.Name);
-                string remMethodName = string.Format("remove_{0}", evt.Name);
+                var qualifiedEventName = evt.Name;
+                var addMethodName = string.Format("add_{0}", evt.Name);
+                var remMethodName = string.Format("remove_{0}", evt.Name);
 
                 //FieldBuilder eFieldBuilder = typeBuilder.DefineField(qualifiedEventName, evt.EventHandlerType, FieldAttributes.Public);
 
-                EventBuilder eBuilder = typeBuilder.DefineEvent(qualifiedEventName, EventAttributes.None, evt.EventHandlerType);
+                var eBuilder = typeBuilder.DefineEvent(qualifiedEventName, EventAttributes.None, evt.EventHandlerType);
 
                 // ADD method
-                MethodBuilder addMethodBuilder = typeBuilder.DefineMethod(addMethodName,
-                    eventMethodAttr, null, new Type[] { evt.EventHandlerType });
+                var addMethodBuilder = typeBuilder.DefineMethod(addMethodName, eventMethodAttr, null, new Type[] { evt.EventHandlerType });
 
                 addMethodBuilder.SetImplementationFlags(eventMethodImpAtr);
 
-                // We need the 'Combine' method from the Delegate type
-                //MethodInfo combineInfo = typeof(Delegate).GetMethod("Combine", new Type[] { typeof(Delegate), typeof(Delegate) });
-
                 // Code generation
-                ILGenerator ilgen = addMethodBuilder.GetILGenerator();
-                //ilgen.Emit(OpCodes.Ldarg_0);
-                //ilgen.Emit(OpCodes.Ldarg_0);
-                //ilgen.Emit(OpCodes.Ldfld, eFieldBuilder);
-                //ilgen.Emit(OpCodes.Ldarg_1);
-                //ilgen.Emit(OpCodes.Call, combineInfo);
-                //ilgen.Emit(OpCodes.Castclass, evt.EventHandlerType);
-                //ilgen.Emit(OpCodes.Stfld, eFieldBuilder);
+                var ilgen = addMethodBuilder.GetILGenerator();
                 ilgen.Emit(OpCodes.Ret);
 
                 // REMOVE method
-                MethodBuilder removeMethodBuilder = typeBuilder.DefineMethod(remMethodName,
-                    eventMethodAttr, null, new Type[] { evt.EventHandlerType });
+                var removeMethodBuilder = typeBuilder.DefineMethod(remMethodName, eventMethodAttr, null, new Type[] { evt.EventHandlerType });
                 removeMethodBuilder.SetImplementationFlags(eventMethodImpAtr);
 
-                MethodInfo removeInfo = typeof(Delegate).GetMethod("Remove", new Type[] { typeof(Delegate), typeof(Delegate) });
+                var removeInfo = typeof(Delegate).GetMethod("Remove", new Type[] { typeof(Delegate), typeof(Delegate) });
 
                 // Code generation
                 ilgen = removeMethodBuilder.GetILGenerator();
-                //ilgen.Emit(OpCodes.Ldarg_0);
-                //ilgen.Emit(OpCodes.Ldarg_0);
-                //ilgen.Emit(OpCodes.Ldfld, eFieldBuilder);
-                //ilgen.Emit(OpCodes.Ldarg_1);
-                //ilgen.Emit(OpCodes.Call, removeInfo);
-                //ilgen.Emit(OpCodes.Castclass, evt.EventHandlerType);
-                //ilgen.Emit(OpCodes.Stfld, eFieldBuilder);
                 ilgen.Emit(OpCodes.Ret);
 
                 // Finally, setting the AddOn and RemoveOn methods for our event
@@ -158,12 +127,10 @@ namespace Nullify
 
                 // Implement the method from the interface
                 typeBuilder.DefineMethodOverride(removeMethodBuilder, evt.GetRemoveMethod(false));
-
-
             }
         }
 
-        private void BuildMethods(TypeBuilder typeBuilder, Type nullifiedType)
+        private void AddMethods(TypeBuilder typeBuilder, Type nullifiedType)
         {
             var methods = nullifiedType.GetMethods();
 
