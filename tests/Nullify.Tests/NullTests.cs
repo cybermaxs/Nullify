819fd9e36b96a1dfdb4a504222ac736661261ebf
Maxime Lemaitre <maxime.lemaitre@outlook.com>
2016-02-01 21:45:14 +0100

    Split interfaces in tests

--- tests/Nullify.Tests/NullTests.cs
+++ tests/Nullify.Tests/NullTests.cs
@@ -7,26 +7,39 @@ namespace Nullify.Tests
     {
         int MyProp { get; set; }
     }
+
+    public interface INullTests2
+    {
+        int MyProp { get; set; }
+    }
+    public interface INullTests3
+    {
+        int MyProp { get; set; }
+    }
+    public interface INullTests4
+    {
+        int MyProp { get; set; }
+    }
     public class NullTests
     {
         [Fact]
         public void Simple_Create()
         {
             //basic
-            var o = Nullified.Of<IBasicProperties>().Create();
+            var o = Nullified.Of<INullTests>().Create();
 
             Assert.NotNull(o);
-            Assert.Contains(typeof(IBasicProperties), o.GetType().GetInterfaces());
+            Assert.Contains(typeof(INullTests), o.GetType().GetInterfaces());
         }
 
         [Fact]
         public void WhenNullOfSameInterface_ShouldCreate2InstancesOfDiffClass()
         {
-            var o1 = Nullified.Of<INullTests>()
+            var o1 = Nullified.Of<INullTests2>()
                     .PropertyOrMethod(x => x.MyProp,1)
                     .PropertyOrMethod(x => x.MyProp,2)
                 .Create();
-            var o2 = Nullified.Of<INullTests>().Create();
+            var o2 = Nullified.Of<INullTests2>().Create();
 
             Assert.NotEqual(o1.GetType(), o2.GetType());
         }
@@ -34,8 +47,8 @@ namespace Nullify.Tests
         [Fact]
         public void WhenNullOfSameInterfaceAndOneisNamed_ShouldCreate2InstancesOftheDiffClass()
         {
-            var o1 = Nullified.Of<INullTests>().Create();
-            var o2 = Nullified.Of<INullTests>().Named("another").Create();
+            var o1 = Nullified.Of<INullTests3>().Create();
+            var o2 = Nullified.Of<INullTests3>().Named("another").Create();
 
             Assert.NotEqual(o1.GetType(), o2.GetType());
         }
@@ -43,8 +56,8 @@ namespace Nullify.Tests
         [Fact]
         public void WhenSameName_ShouldCreate2InstancesOftheSameClass()
         {
-            var o1 = Nullified.Of<INullTests>().Named("another").Create();
-            var o2 = Nullified.Of<INullTests>().Named("another").Create();
+            var o1 = Nullified.Of<INullTests4>().Named("another").Create();
+            var o2 = Nullified.Of<INullTests4>().Named("another").Create();
 
             Assert.Equal(o1.GetType(), o2.GetType());
         }
--- tests/Nullify.Tests/SetupTests.cs
+++ tests/Nullify.Tests/SetupTests.cs
@@ -16,36 +16,24 @@ namespace Nullify.Tests
         {
             Nullified
                 .Of<IBasicProperties>()
-                .Dependency<INestFirstLevel>(s=>
+                .Dependency<INestFirstLevel>(s =>
                 {
                     s.PropertyOrMethod(a => a.Sub, null);
                 })
                 .Create();
-               // .Type<IBasicMethods>(d=>d.PropertyOrMethod())
-
-            //var o1 = Nullified
-            //    .Of<ISetupTests>()
-            //    .Type
-            //    .PropertyOrMethod(i => i.MyProp).Returns(5)
-            //    .Named("base1")
-            //    .Create();
-
-            //Assert.Equal(5, o1.MyProp);
 
             var o2 = Nullified.Of<ISetupTests>()
                .PropertyOrMethod(i => i.MyProp, 15)
                 .Named("base2")
                 .Create();
             Assert.Equal(15, o2.MyProp);
-
-            //Assert.NotEqual(o1, o2);
         }
 
         [Fact]
         public void WhenNotInteface_ShouldCreateNull()
         {
             var setup = Nullified.Of<Version>();
-               
+
             Assert.False(setup.CanCreate());
 
             Assert.Null(setup.Create());
